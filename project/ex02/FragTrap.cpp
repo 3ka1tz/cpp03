5c8c3174fbc245ae8ff82aff5ff5d96f82192a6c
3ka1tz <ekaitzlopez2025@gmail.com>
2025-11-11 12:38:57 +0100

    making improvements

--- project/ex02/FragTrap.cpp
+++ project/ex02/FragTrap.cpp
@@ -1,50 +1,55 @@
 #include "FragTrap.hpp"
+
 #include <iostream>
 
 FragTrap::FragTrap() : ClapTrap("Default") {
-    hitPoints = 100;
-    energyPoints = 50;
-    attackDamage = 30; 
-    std::cout << "FragTrap default constructor called" << std::endl;
+    _hitPoints = 100;
+    _energyPoints = 50;
+    _attackDamage = 30;
+
+    std::cout << "FragTrap " << _name << " constructed" << std::endl;
 }
 
 FragTrap::FragTrap(const std::string& name) : ClapTrap(name) {
-    hitPoints = 100;
-    energyPoints = 50;
-    attackDamage = 30;
-    std::cout << "FragTrap constructor called for " << name <<  std::endl;
+    _hitPoints = 100;
+    _energyPoints = 50;
+    _attackDamage = 30;
+
+    std::cout << "FragTrap " << _name << " constructed" << std::endl;
 }
 
 FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other) {
-    std::cout << "FragTrap copy constructor called for " << name << std::endl;
+    std::cout << "FragTrap copy constructor called" << std::endl;
 }
 
 FragTrap& FragTrap::operator=(const FragTrap& other) {
+    std::cout << "FragTrap copy assignment operator called" << std::endl;
+
     ClapTrap::operator=(other);
-    std::cout << "FragTrap copy assignment operator called for " << name << std::endl;
     return *this;
 }
 
 FragTrap::~FragTrap() {
-    std::cout << "FragTrap destructor called for " << name << std::endl;
+    std::cout << "FragTrap " << _name << " destructed" << std::endl;
 }
 
 void FragTrap::attack(const std::string& target) {
-    if (hitPoints <= 0) {
-        std::cout << "FragTrap " << name << " can't attack because it has no hit points left" << std::endl;
+    if (_hitPoints <= 0) {
+        std::cout << "FragTrap " << _name << " can't attack because it has no hit points left" << std::endl;
         return;
     }
 
-    if (energyPoints <= 0) {
-        std::cout << "FragTrap " << name << " can't attack because it has no energy points left" << std::endl;
+    if (_energyPoints <= 0) {
+        std::cout << "FragTrap " << _name << " can't attack because it has no energy points left" << std::endl;
         return;
     }
 
-    std::cout << "FragTrap " << name << " attacks " << target << ", causing " << attackDamage << " points of damage" << std::endl;
-    energyPoints--;
-    std::cout << "FragTrap " << name << " now has " << energyPoints << " energy points left" << std::endl;
+    std::cout << "FragTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage" << std::endl;
+
+    _energyPoints--;
+    std::cout << "FragTrap " << _name << " now has " << _energyPoints << " energy points left" << std::endl;
 }
 
 void FragTrap::highFivesGuys() {
-    std::cout << "FragTrap " << name << " is making a positive high-fives request" << std::endl;
+    std::cout << "FragTrap " << _name << " is making a positive high-fives request" << std::endl;
 }
